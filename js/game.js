992a8d91a1db6409a925a0362f7af171b9982235
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-11-11 18:27:27 +0100

    playerModule IIFE function done which holds information about each player and currenTurns.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,32 @@
+// All module imports
+const readline = require("readline");
+
+// Create the players
+const playerModule = (() => {
+    // create players array
+    const players = {
+        X: {token: "X", turn: true},
+        O: {token: "O", turn: false},
+    };
+
+    const currentTurn = players.X;
+
+    // Getter functions for getting the player and the player with current turn
+    const getPlayerToken = (token) => {
+        if(token in players) {
+            return players[token].token;
+        } else 
+            return `Error! Player ${token} doesn't exist.`;
+    };
+    // This just returns the currentTurn variable
+    const getTurn = () => currentTurn;
+
+    return {
+        getPlayerToken,
+        getTurn,
+    };
+})();
+
 // Function to create cells containing object holding information about token
 const createCell = () => {
     const token = null;
@@ -21,5 +50,23 @@ const generateBoard = () => {
     return gameboard;
 };
 
-const board = generateBoard();
-console.log(board);
+// Beacuse its the CLI version, we need to receive the input with readline
+const getMove = () => {
+ const createRL = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.question("");
+ };
+
+};
+
+const gameController = () => {
+    let movesLeft = 9;
+    while (movesLeft > 0 && !winner) {
+        //  switch turn between players
+
+    }
+};
